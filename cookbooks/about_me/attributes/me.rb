df7faa3a0e13ce8099bd8a041f92a4f18d476ee6
jameso1 <jodonnell@offis.com.au>
2014-10-27 04:35:39 +0000

    Added the about_me cookbook

---
+++ cookbooks/about_me/attributes/me.rb
@@ -0,0 +1,4 @@
+set[:about_me][:first_name] = "Lucifer"
+default[:about_me][:favorite][:roman] = "Pilate"
+default[:about_me][:favorite][:romanov] = "Anastasia"
+default[:about_me][:favorite][:transport] = "tank"
---
+++ cookbooks/about_me/recipes/default.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: about_me
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+rightscale_marker
+
+  first_name = node[:about_me][:first_name]
+  roman = node[about_me][:favorite][:roman]
+  romanov = node[:about_me][:favorite][:romanov]
+  transport = node[:about_me][:favorite][:transport]
+
+  log "Just call me #{first_name} and I like #{roman}, #{romanov} and travelling by #{tank}."
+
