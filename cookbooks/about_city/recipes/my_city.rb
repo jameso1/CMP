11b63b7111dadf39b9e7967e1766d2824517e31d
jameso1 <jodonnell@offis.com.au>
2014-10-28 00:47:36 +0000

    Corrected about_city cookbook

--- cookbooks/about_city/recipes/my_city.rb
+++ cookbooks/about_city/recipes/my_city.rb
@@ -4,4 +4,4 @@ rightscale_marker
     temperature = node[:about_city][:temperature]
       rain = node[:about_city][:rain]
 
-        log "I live in #{city_name}. The current temperature is #temperature degrees. And if you're wondering if you need and umbrella, the answer is #{rain}."
+        log "I live in #{city_name}. The current temperature is #{temperature} degrees. And if you're wondering if you need and umbrella, the answer is #{rain}."
