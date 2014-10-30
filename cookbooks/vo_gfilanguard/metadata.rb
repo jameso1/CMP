8a482ec4f603c1dae8d50c01f0d6e1401c651629
jameso1 <jodonnell@offis.com.au>
2014-10-30 01:57:47 +0000

    Added the vo_gfilanguard cookbook

---
+++ cookbooks/vo_gfilanguard/metadata.rb
@@ -0,0 +1,11 @@
+name             'vo_gfilanguard'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures vo_gfilanguard'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends "rightscale"
+
+recipe "vo_gfilanguard::install_gfilanguard","Dummy placeholder for gfilanguard installation."
