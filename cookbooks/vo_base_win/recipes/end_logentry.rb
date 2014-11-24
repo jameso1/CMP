b97180a5b1094fad584a4e6e020f38d0cafa6bb0
jameso1 <jodonnell@offis.com.au>
2014-11-24 21:57:44 +0000

    Corrected missing concatenation in end_logentry

--- cookbooks/vo_base_win/recipes/end_logentry.rb
+++ cookbooks/vo_base_win/recipes/end_logentry.rb
@@ -4,7 +4,7 @@
 powershell "end_logentry" do
 
   powershell_script = <<'POWERSHELL_SCRIPT'
-  echo "Completion mark for Offis base installation." > c:\vo_log.txt
+  echo "Completion mark for Offis base installation." >> c:\vo_log.txt
 
 POWERSHELL_SCRIPT
   source(powershell_script)
