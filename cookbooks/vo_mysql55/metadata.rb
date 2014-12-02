fcb3a022435952aa82dcd3f66877530b14affb74
jameso1 <jodonnell@offis.com.au>
2014-12-02 02:28:14 +0000

    Add the MySQL 5.5 cookbook

---
+++ cookbooks/vo_mysql55/metadata.rb
@@ -0,0 +1,12 @@
+name             'vo_mysql55'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures vo_mysql55'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends "rightscale"
+
+recipe "vo_mysql55::install_mysql55","Dummy placeholder for MySQL 5.5 installation."
+
---
+++ cookbooks/vo_mysql55/recipes/install_mysql55.rb
@@ -0,0 +1,3 @@
+rightscale_marker
+
+  log "Dummy place keeper: Install mysql5.5 installation code here."
