032949d1da5d68cc9de33a47b0dc44e209876e5b
jameso1 <jodonnell@offis.com.au>
2014-12-03 21:26:07 +0000

    Commit vo_mysql modifications post adding mysql

--- cookbooks/vo_mysql55/metadata.rb
+++ cookbooks/vo_mysql55/metadata.rb
@@ -7,6 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends "rightscale"
+depends "mysql", "4.1.2"
 
 recipe "vo_mysql55::install_mysql55","Dummy placeholder for MySQL 5.5 installation."
 
--- cookbooks/vo_mysql55/recipes/install_mysql55.rb
+++ cookbooks/vo_mysql55/recipes/install_mysql55.rb
@@ -1,3 +1,7 @@
 rightscale_marker
 
   log "Dummy place keeper: Install mysql5.5 installation code here."
+  log "For sake of example am adding mysql 4.1.2."
+
+  include_recipe "mysql::client"
+  include_recipe "mysql::server"
