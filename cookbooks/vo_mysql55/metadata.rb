57c73b04e4ae868e3e3e18977b4a8e122086d065
jameso1 <jodonnell@offis.com.au>
2014-12-04 04:19:08 +0000

    Updating attributes as inputs

--- cookbooks/vo_mysql55/metadata.rb
+++ cookbooks/vo_mysql55/metadata.rb
@@ -11,3 +11,19 @@ depends "mysql", "4.1.2"
 
 recipe "vo_mysql55::install_mysql55","Dummy placeholder for MySQL 5.5 installation."
 
+# Required Inputs
+
+attribute "mysql/server_debian_password",
+   :display_name => "Debian password",
+   :description => "Value to assign to debian password",
+   :required => "required"
+
+attribute "mysql/server_root_password",
+   :display_name => "root password",
+   :description => "Value to assign to root password",
+   :required => "required"
+
+attribute "mysql/server_repl_password",
+   :display_name => "Replication password",
+   :description => "Value to assign to replication password",
+   :required => "required"
