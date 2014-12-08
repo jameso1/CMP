93a5424203e4f38ee51ec09690e6d513f2976446
jameso1 <jodonnell@offis.com.au>
2014-12-08 05:20:21 +0000

    Adding SQL Server 2012 cookbook

---
+++ cookbooks/vo_sqlserver2012/metadata.rb
@@ -0,0 +1,11 @@
+name             'vo_sqlserver2012'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures vo_sqlserver2012'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends "rightscale"
+
+recipe "vo_sqlserver2012::install_sqlserver2012","Dummy placeholder for SQL Server 2012 installation."
---
+++ cookbooks/vo_sqlserver2012/recipes/install_sqlserver2012.rb
@@ -0,0 +1,3 @@
+rightscale_marker
+
+  log "Dummy place keeper: Install SQL Server 2012 installation code here."
